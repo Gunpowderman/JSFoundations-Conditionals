599a7fd83d14625a208248d5e6d60bb16dd165e7
Gunpowderman <adel.baroody@gmail.com>
2021-02-02 18:31:51 +0300

    Solved the Conditionals task.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,25 @@
 // Define the numbers and the operation here
 
+let Firstnumber = 8;
+let Secondnumber = 5;
+let Operation = "*";
+
+
 // Write if-statements here
+
+if (Operation == "+")
+{
+    console.log (Firstnumber + Secondnumber);
+}
+else if (Operation == "-")
+{
+    console.log (Firstnumber - Secondnumber);
+}
+else if (Operation == "*")
+{
+    console.log (Firstnumber * Secondnumber);
+}
+else if (Operation == "/")
+{
+    console.log (Firstnumber / Secondnumber);
+}
